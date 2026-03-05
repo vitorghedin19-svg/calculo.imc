2b597ca5051c66ba00b330826d5dd0cb73a26528
VITOR CASAGRANDE GHEDIN <vitor.ghedin@alunos.sc.senac.br>
2026-03-05 20:14:59 -0300

    primeiro commit

---
+++ src/test/java/br/com/senac/imc/ImcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.imc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
